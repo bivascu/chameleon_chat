4e898f118fe10303a6f54ce74520728595aa5385
code_isle <code_isle@gmail.com>
2025-06-06 12:14:42 -0400

    Intial commit

---
+++ src/main/java/com/oaks/chameleon_chat/ChameleonChatApplication.java
@@ -0,0 +1,13 @@
+package com.oaks.chameleon_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChameleonChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChameleonChatApplication.class, args);
+    }
+
+}
